7fe944ed625dc7da29d343cc77fbcdfa56ad976c
alonavns <alona.vereshaka@gmail.com>
2020-08-21 19:55:20 +0300

    support - alona

--- src/components/ConstantWeather/ConstantWeather.js
+++ src/components/ConstantWeather/ConstantWeather.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import classes from './ConstantWeather.css'
+import './ConstantWeather.css' // no need named import for classes >>>> classes from 'xx'
 
 function constantWeather(props) {
     return (
-        <div className={classes.Card} key={props.cityId}>
+        <div className="Card">
+            {/*
+                - no need to provide key here. >>>> key={props.cityId}
+                - need to write string as className
+            */}
             <div>{props.cityName}, {props.country}</div>
             <div>{props.temperature}</div>
             <img />
